7bbf893ce4742f8ff014d231bdf796b55991d53d
Christina Phanekham <44453826+ctinap@users.noreply.github.com>
2020-03-08 22:44:03 -0700

    Started template for results page and added a temporary redirect to the results page after clicking start

--- BioMF/Startup.cs
+++ BioMF/Startup.cs
@@ -28,7 +28,7 @@ namespace BioMF
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            //services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
