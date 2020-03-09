9255bfed886d3204c8884061f57cc8c26582efa7
David Mehedinti <49506223+DM1224@users.noreply.github.com>
2020-03-09 15:59:48 -0700

    Reintroducing ParaMODA Files [ADDED] All projects necessary for ParaMODA to work [MODIFIED] SLN file now references ParaMODA project files, solving previous incorrect import procedures
    NOTE: Everything should compile now. The issue was that the ParaMODA project files were not formally added to the SLN file.

---
+++ BioMF/Data/QuickGraph/Algorithms/IVertexColorizerAlgorithm.cs
@@ -0,0 +1,13 @@
+﻿namespace QuickGraph.Algorithms
+{
+    public interface IVertexColorizerAlgorithm<TVertex,TEdge>
+        where TEdge : IEdge<TVertex>
+    {
+        GraphColor GetVertexColor(TVertex v);
+    }
+
+    public interface IVertexColorizerAlgorithm<TVertex>
+    {
+        GraphColor GetVertexColor(TVertex v);
+    }
+}
---
+++ BioMF/Data/QuickGraph/Collections/EdgeList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickGraph.Collections
+{
+#if !SILVERLIGHT
+    [Serializable]
+#endif
+    public sealed class EdgeList<TVertex>
+        : List<Edge<TVertex>>
+        , IEdgeList<TVertex>
+#if !SILVERLIGHT
+        , ICloneable
+#endif
+    {
+        public EdgeList() 
+        { }
+
+        public EdgeList(int capacity)
+            : base(capacity)
+        { }
+
+        public EdgeList(EdgeList<TVertex> list)
+            : base(list)
+        {}
+
+        public EdgeList<TVertex> Clone()
+        {
+            return new EdgeList<TVertex>(this);
+        }
+
+        IEdgeList<TVertex> IEdgeList<TVertex>.Clone()
+        {
+            return this.Clone();
+        }
+
+#if !SILVERLIGHT
+        object ICloneable.Clone()
+        {
+            return this.Clone();
+        }
+#endif
+    }
+}
