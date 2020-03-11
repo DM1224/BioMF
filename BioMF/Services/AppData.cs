95ad0c4b1ae0ff48a87b1afe1b3991fc3f1f8762
Christina Phanekham <44453826+ctinap@users.noreply.github.com>
2020-03-10 18:11:50 -0700

    Fixed formatting of results

--- BioMF/Services/AppData.cs
+++ BioMF/Services/AppData.cs
@@ -12,7 +12,6 @@ namespace BioMF.Services
 {
     public class AppData
     {
-        public string Hah { get; set; }
         public string[] Network { get; set; } //since we cant use system io, having a string of networks can start the process
         public int SubgraphSize { get; set; } = 3; //default value is 3
 
@@ -20,9 +19,10 @@ namespace BioMF.Services
 
         public StringBuilder sb = new StringBuilder(""); //paramoda uses a stringbuilder for the output
         
+        public List<string> SubgraphResults = new List<string>(); //Used to output in results
+        public int Mappings { get; set; } = 0;
         public IFileListEntry NetworkFile { get; set; }
         public UndirectedGraph<int> InputGraph { get; set; }
-
         public Dictionary<QueryGraph, ICollection<Mapping>> FrequentSubgraphs { get; set; }
     }
 }
